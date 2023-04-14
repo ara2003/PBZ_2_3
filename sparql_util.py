97fb27b7f46696986eb0ef5bc28b26080f575911
Arseny Latyshev <arseny.latyshev@gmail.com>
2023-04-14 16:21:22 +0300

    Update sparql_util.py

--- sparql_util.py
+++ sparql_util.py
@@ -4,6 +4,9 @@ from raw_sparql_util import *
 def create_triple(subject, predicate, object):
     return update('INSERT DATA {' + f'{subject.__str__()} {predicate.__str__()} {object.__str__()}' + '}')
 
+def get_all_triple(subject, predicate, object):
+    return query('SELECT * WHERE { ?subject ?predicate ?object }')
+
 m  = Node('http://www.w3.org/2023/03/31-my#maks')
 n  = Node('http://www.w3.org/2023/03/31-my#name')
 ml = Literal('Maksim')
@@ -20,13 +23,7 @@ print(create_triple(m, n, ml))
 
 # """)
 
-result = query("""
-
-SELECT * WHERE {
-  ?subject ?predicate ?obj
-}
-
-""")
+result = get_all_triple()
 
 print(result.names())
 for row in result.rows():
