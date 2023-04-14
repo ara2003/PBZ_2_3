056508889b74e1ed769d3f53a85373dca6935a58
Bunnyhug01 <mkavkovv@gmail.com>
2023-04-14 16:26:53 +0300

    Add Django project

---
+++ OntologyApp/main/row_sparql_util.py
@@ -0,0 +1,83 @@
+from SPARQLWrapper import *
+
+database_url="http://localhost:3030/test"
+database_prefix ="""
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX owl: <http://www.w3.org/2002/07/owl#>
+PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+PREFIX : <http://www.w3.org/2023/03/31-my#>
+"""
+
+class Resource:
+    pass
+
+class Node(Resource):
+    def __init__(self, uri: str):
+        self.uri = uri
+
+    def __repr__(self):
+        return f"Node({self.uri})"
+
+class Literal(Resource):
+    def __init__(self, value: str):
+        self.value = value
+
+    def __repr__(self):
+        return f"Literal({self.value})"
+
+class Triple:
+    def __init__(self, subject, predicate, object):
+        self.subject = subject
+        self.predicate = predicate
+        self.object = object
+
+    def __repr__(self):
+        return f"Triple({self.subject}, {self.predicate}, {self.object})"
+
+class ResultSet:
+    def __init__(self, json_result) -> None:
+        self.json_result = json_result
+    
+    def names(self):
+        return self.json_result['head']['vars']
+
+    def rows(self):
+        return list(map(parse_result_row, self.json_result['results']['bindings']))
+
+def parse_resource(resource):
+    type = resource["type"]
+    if type == "uri":
+        return Node(resource["value"])
+    if type == "literal":
+        return Literal(resource["value"])
+    raise Exception("type: {type}")
+        
+def parse_result_row(row: dict):
+    result = {}
+    for n in row.keys():
+        result[n] = parse_resource(row[n])
+    return result
+
+def query(query: str) -> ResultSet:
+    query = database_prefix + query
+    sparql = SPARQLWrapper(database_url)
+    sparql.setReturnFormat(JSON)
+    sparql.setQuery(query)
+
+    ret = sparql.query().convert()
+    result = ResultSet(ret)
+    return result
+
+def update(query: str) -> dict[str, str]:
+    query = database_prefix + query
+    sparql = SPARQLWrapper(database_url)
+    sparql.setReturnFormat(JSON)
+    sparql.setMethod(POST)
+    sparql.setHTTPAuth(DIGEST)
+    sparql.setQuery(query)
+    query = sparql.query()
+    result = query.convert()
+    if result["statusCode"] != 200:
+        raise Exception(result["message"])
+    return result
---
+++ OntologyApp/main/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from sparql_util import *
+
+# Create your views here.
+
+def ontology(request):
+    pass
